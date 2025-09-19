10a0b5b8bd51fd49996535f1d6814a70602fb9b7
ianlau <ianlauyin@gmail.com>
2025-09-19 11:41:38 +0800

    migrate admin routes for core-rs

--- backend/src/ajax/router/admin.rs
+++ backend/src/ajax/router/admin.rs
@@ -1,11 +1,13 @@
 use axum::{
     extract::{ConnectInfo, State},
-    http::{header::USER_AGENT, HeaderMap, StatusCode},
+    http::{header::USER_AGENT, HeaderMap},
     routing::{get, post},
     Router,
 };
 
-use framework::web::body::Json;
+use framework::exception;
+use framework::exception::error_code::BAD_REQUEST;
+use framework::web::{body::Json, error::HttpResult};
 use std::net::SocketAddr;
 
 use crate::ajax::{
@@ -25,34 +27,39 @@ pub fn admin_router() -> Router<SharedState> {
 async fn get_login_record(
     State(state): State<SharedState>,
     header: HeaderMap,
-) -> (StatusCode, Json<Option<LoginResponse>>) {
-    if let Some(token) = get_cookie(&header, CookieName::LoginToken) {
-        if let Some(record) = AdminRecordCollection::from(state.db.clone())
-            .get_record(token)
-            .await
-            .expect("Failed to get login record")
-        {
-            return (StatusCode::OK, Json(Some(record.into())));
-        }
+) -> HttpResult<Json<LoginResponse>> {
+    let Some(token) = get_cookie(&header, CookieName::LoginToken) else {
+        return Err(exception!(code = BAD_REQUEST, message = "Token not found"))?;
     };
 
-    (StatusCode::NOT_FOUND, Json(None))
+    let Some(record) = AdminRecordCollection::from(state.db.clone())
+        .get_record(token)
+        .await
+        .expect("Failed to get login record")
+    else {
+        return Err(exception!(
+            code = BAD_REQUEST,
+            message = "Login record not found"
+        ))?;
+    };
+
+    Ok(Json(record.into()))
 }
 
+#[axum::debug_handler]
 async fn login(
     State(state): State<SharedState>,
     request_header: HeaderMap,
     ConnectInfo(addr): ConnectInfo<SocketAddr>,
     Json(request): Json<LoginRequest>,
-) -> (StatusCode, HeaderMap, Json<Option<LoginResponse>>) {
+) -> HttpResult<(HeaderMap, Json<LoginResponse>)> {
     let expected_password = env::var("LOGIN_PASSWORD");
 
-    let mut header = HeaderMap::new();
-
     if request.password != expected_password {
-        return (StatusCode::UNAUTHORIZED, header, Json(None));
+        Err(exception!(code = BAD_REQUEST, message = "Invalid password"))?;
     }
 
+    let mut header = HeaderMap::new();
     let user_agent = request_header.get(USER_AGENT).unwrap().to_str().unwrap();
     let ip_address = addr.ip().to_string();
 
@@ -67,5 +74,5 @@ async fn login(
         &record.token().to_string(),
     );
 
-    (StatusCode::OK, header, Json(Some(record.into())))
+    Ok((header, Json(record.into())))
 }
