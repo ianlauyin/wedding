292b19677cd907308c17aecea10c9b976cf9402b
ianlau <ianlauyin@gmail.com>
2025-09-22 08:24:46 +0800

    update using asset_path from core_rs

--- backend/src/website.rs
+++ backend/src/website.rs
@@ -1,11 +1,9 @@
-use std::path::PathBuf;
-
-use framework::exception::Exception;
+use framework::{asset::asset_path, exception::Exception};
 use tower_http::services::{ServeDir, ServeFile};
 
 // Update after frontend ready
 pub fn html() -> Result<ServeDir<ServeFile>, Exception> {
-    let asset_path = PathBuf::from("../assets/web/");
+    let asset_path = asset_path("assets/web")?;
     let index_path = asset_path.join("index.html");
     Ok(ServeDir::new(asset_path).fallback(ServeFile::new(index_path)))
 }
