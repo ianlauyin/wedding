ecccc1f58a16ddee1fa3ea2bd36f52cdfae41393
ianlau <ianlauyin@gmail.com>
2025-10-02 15:21:00 +0800

    update Loading component

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -1,3 +1,7 @@
 export const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div class="flex justify-center items-center h-full w-full">
+      <div class="w-12 h-12 border-4 rounded-full animate-spin border-t-gray-400"></div>
+    </div>
+  );
 };
