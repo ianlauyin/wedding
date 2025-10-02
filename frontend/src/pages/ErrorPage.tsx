286292a1d5d57045385be2bf325ea716dbd55891
ianlau <ianlauyin@gmail.com>
2025-10-02 15:36:02 +0800

    update ErrorPage

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,14 +1,23 @@
+import { Flex } from "~/solidui/components/flex";
+
 export interface Props {
   message: string;
 }
 
 export const ErrorPage = ({ message }: Props) => {
   return (
-    <div>
-      <h4>Oops!</h4>
-      <h5>Something went wrong :(</h5>
-      <p>Error: {message}</p>
-      <p>Please contact Ian/Claire if we sent you here</p>
-    </div>
+    <Flex
+      flexDirection="col"
+      justifyContent="center"
+      alignItems="center"
+      class="h-full w-full space-y-4"
+    >
+      <h1 class="text-5xl font-bold">Oops!</h1>
+      <h2 class="text-3xl font-bold">Something went wrong :(</h2>
+      <div>
+        <p>Error: {message}</p>
+        <p>Please contact Ian/Claire if we sent you here</p>
+      </div>
+    </Flex>
   );
 };
