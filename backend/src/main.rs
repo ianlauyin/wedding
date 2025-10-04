6061863eb36704f026d853b5c03a96c150a2c4c3
ianlau <ianlauyin@gmail.com>
2025-10-04 12:21:14 +0800

    remove log record logger

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,14 @@
-use framework::log;
+use framework::log::{self, ConsoleAppender};
 use framework::shutdown::Shutdown;
 use framework::web::server::{HttpServerConfig, start_http_server};
 
-use crate::{ajax::ajax_router, db::connect_db, logger::FirestoreAppender};
+use crate::{ajax::ajax_router, db::connect_db};
 use crate::{exception::CoreRsResult, state::AppState};
 
 mod ajax;
 mod db;
 mod env;
 mod exception;
-mod logger;
 mod state;
 mod website;
 
@@ -19,9 +18,9 @@ async fn main() -> CoreRsResult<()> {
     let signal = shutdown.subscribe();
     shutdown.listen();
 
-    let db = connect_db().await?;
-    log::init_with_action(FirestoreAppender::new(db.clone()));
+    log::init_with_action(ConsoleAppender);
 
+    let db = connect_db().await?;
     let state = AppState::init(db.clone());
 
     let app = axum::Router::new();
