e899d396c1c5f022232e3f318847aed1065d796f
ianlau <ianlauyin@gmail.com>
2025-10-09 15:51:26 +0800

    update future features list

--- frontend/src/pages/Internal/Backoffice/FutureFeatureList.tsx
+++ frontend/src/pages/Internal/Backoffice/FutureFeatureList.tsx
@@ -3,7 +3,6 @@ export const FutureFeatureList = () => {
     <div>
       <h4 class="text-lg font-bold">Future Features</h4>
       <ul class="steps steps-vertical">
-        <li class="step">Update Guest Detail</li>
         <li class="step">Generate Invitation address</li>
         <li class="step">Sort Guest</li>
         <li class="step">Search Guest</li>
